af6494c2a0d127a86dc04ac57aafb85c5af6f5b4
Ismael Fernandez <69177486+ifcodesdotnet@users.noreply.github.com>
2024-12-20 13:50:17 -0600

    InProgress Cross-Platform PowerShell Scripts

--- powershell-scripts/Update-Dotfiles.ps1
+++ powershell-scripts/Update-Dotfiles.ps1
@@ -1,21 +1,22 @@
-Import-Module $([System.IO.Path]::Combine("$env:USERPROFILE", ".iffiles", "powershell-modules","Configuration-Management.psm1"))
+Import-Module $([System.IO.Path]::Combine("$env:IF_HOME", ".iffiles", "powershell-modules","Configuration-Management.psm1"))
 
 function main {
-    $configurations = Get-Content -Path $([System.IO.Path]::Combine("$env:USERPROFILE", ".iffiles", "configurations.json")) -Raw | ConvertFrom-Json
+    foreach($item in $(Get-ConfigurationItemsByPlatform)) {
 
-    foreach($configuration in $configurations) {
-
-        if($null -ne $($configuration.'dependencies')) {
-            $dependencies = Get-Dependency -name $($configuration.'name') -configurations $configurations
-
+        if($null -ne $($item.dependencies)) {
+            $dependencies = Get-Dependency -name $($item.'name')
             foreach($dependency in $dependencies) {
-                $dependant = Get-Configuration -name $dependency -configurations $configurations
+                $dependant = Get-PlatformConfigurationByItemName -name $dependency
+
                 Update-Configuration -name $($dependant.'name') -target $($dependant.'target-directory')
             }
         }
 
-        Update-Configuration -name $($configuration.'name') -target $($configuration.'target-directory')
+        $platform = Get-PlatformConfigurationByItemName -name $item.name
+
+        Update-Configuration -name $($item.'name') -target $($platform.'target-directory')
     }
 }
 
+# todo, test this script...
 main
